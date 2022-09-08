0eff1ea07c5142ed5b7ce7f697b4b21a3705aa8d
jyotiprakash722 <dasjyotiprakash722@gmail.com>
2022-09-08 20:48:25 +0530

    Update Basic Creation & Insertion.sql

--- Basic Creation & Insertion.sql
+++ Basic Creation & Insertion.sql
@@ -1,20 +1,20 @@
-create table student (
-roll char(8),
-name varchar(20),
-age int
-);
-insert into student value(
-'19DIT195','Jyotiprakash Das',20
-);
-select * from student;
-create table library(
-roll char(8),
-book_taken varchar(30),
-total_fine int
+CREATE TABLE student (
+    roll CHAR(8),
+    name VARCHAR(20),
+    age INT
 );
 
-insert into library values(
-'19DIT195', 'none',0
+describe student;
+
+CREATE TABLE library (
+    roll CHAR(8),
+    book_taken VARCHAR(30),
+    total_fine INT
 );
 
-select * from library;
+describe library;
+
+SELECT 
+    *
+FROM
+    library;
