99d8321e9dbd477128c01dd3437d6e99be0b5fd1
jyotiprakash722 <dasjyotiprakash722@gmail.com>
2022-09-16 10:56:07 +0530

    Commit

---
+++ Creating a basic Student Table.sql
@@ -0,0 +1,8 @@
+# All comments written, Followed by hash symbol.
+CREATE TABLE student (
+    Student_ID INT PRIMARY KEY,
+    Name VARCHAR(20) NOT NULL,
+    Major VARCHAR(20)
+);
+
+describe student; #It describes the structure of the student table.
---
+++ Inserting some data into Student Table.sql
@@ -0,0 +1,10 @@
+INSERT INTO student (Student_ID, Name, Major) VALUES
+(1, "Jack", "Biology"),
+(2, "Kate", "Sociology"),
+(3, "Claire", "English"),
+(4, "Jack", "Biology"),
+(5, "Mike", "Computer Science");
+
+describe table student;
+
+select * from student;
